a58ae7b0f8df821c4c18b44e6750764cd8d73f6d
WeslleyAndrik <weslleyandrik2107@gmail.com>
2023-11-15 00:51:07 -0300

    Mensagem de confirmação
    Mensagem de confirmação de envio de fromulário.

--- faleconosco.php
+++ faleconosco.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fale Conosco</title>
+    <link rel="stylesheet" href="styles/faleconosco.css">
+</head>
+<header class="logo">
+    <h1><img src="images/png-FESTIFY.png" width="200"></h1>
+</header> 
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Receber dados do formulário
@@ -21,3 +33,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 ?>
+
+<body>   
+    <form action="#" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="motivo">Motivo de Contato:</label>
+        <select id="motivo" name="motivo" required>
+            <option value="" disabled selected>Selecione...</option>
+            <option value="duvida">Dúvida</option>
+            <option value="feedback">Feedback</option>
+            <option value="problema">Problema</option>
+            <option value="outro">Outro</option>
+        </select>
+
+        <label for="mensagem">Mensagem:</label>
+        <textarea id="mensagem" name="mensagem" rows="4" required></textarea>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
--- obrigado.php
+++ obrigado.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Obrigado!</title>
+    <title>Obrigado! </title>
     <link rel="stylesheet" href="styles/faleconosco.css">
 </head>
 <body>
-    <h1>Obrigado!</h1>
+    <h1>Obrigado! </h1>
     <p>Recebemos sua mensagem. Qualquer coisa entraremos em contato e agradecemos sua mensagem.</p>
     <!-- Depois a gente altera a mensagem qualquer coisa -->
 </body>
